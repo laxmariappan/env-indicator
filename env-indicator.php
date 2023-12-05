704ead5440d6868bc6978418ab3da3066bc36862
Lax Mariappan <lax.mariappan@webdevstudios.com>
2023-12-05 14:22:22 +0530

    add plugin headers

---
+++ env-indicator.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * @wordpress-plugin
+ * Plugin Name: Environment Indicator.
+ * Description: Shows the current environment on WP admin bar.
+ * Version: 1.0.0
+ * License: GPLv2+
+ * Requires at least: 6.3
+ * Requires PHP: 8.0
+ * Update URI: false
+ */
